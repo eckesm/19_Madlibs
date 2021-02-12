8a95a7908c6e3c087def1bf8a15d7846e6ca7b48
Matthew Eckes <eckesm@gmail.com>
2021-02-12 18:18:52 -0500

    completed assignment, consulted solutions, might do additional further study at a later time

--- app.py
+++ app.py
@@ -1,3 +1,37 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from random import sample
 import stories
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret_key_02-12-2021_14-22-44'
+debug = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def show_story_selection():
+    stories_list = stories.STORIES
+    return render_template('story_selection.html', stories=stories_list)
+
+
+@app.route('/form')
+def show_form():
+    story_id = request.args['story']
+    story = stories.dict_stories_by_id[story_id]
+    
+    prompts = story.prompts
+    randomized_prompts = sample(prompts, len(prompts))
+    
+    return render_template('form.html', prompts=randomized_prompts, story=story)
+
+
+@app.route('/story')
+def show_story():
+    story_id = request.args['story']
+    story = stories.dict_stories_by_id[story_id]
+    
+    answers = dict(request.args)
+    completed_story = story.generate(answers)
+
+    return render_template('story.html', completed_story=completed_story, answers=answers)
--- stories.py
+++ stories.py
@@ -18,9 +18,11 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, story_id, description, words, text,):
         """Create story with words and template text."""
 
+        self.story_id = story_id
+        self.description = description
         self.prompts = words
         self.template = text
 
@@ -36,10 +38,31 @@ class Story:
 
 
 # Here's a story to get you started
+story001 = Story('001',
+                 'Once upon a time...',
+                 ["place", "noun", "verb", "adjective", "plural noun"],
+                 """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural noun}."""
+                 )
+
+# STORY TEMPLATE FROM:
+# https://thoughtcatalog.com/january-nelson/2018/06/funny-stories/
+# accessed: 2/12/2021
+
+story002 = Story('002',
+                 'A Date to Forget',
+                 ['name', 'place', 'noun-1', 'noun-2',
+                  'verb(ing)-1', 'noun-3', 'body part-1', 'noun-4', 'verb(ing)-2', 'body part-2', 'possessive pronoun'],
+                 """In my junior year of high school, {name} asked me on a date to {place}. {name} rented a {noun-1} and we made a {noun-2}. We were {verb(ing)-1} the {noun-1} and the {noun-3} beeped so the {noun-2} was done. {name} looked me dead in the eye and said, “This is the worst part.” I then watched {name} open the {noun-3} and pull the {noun-2} out with {possessive pronoun} bare {body part-1}, {noun-4} and all, {verb(ing)-2} at the top of his {body part-2}. We never had a second date."""
+                 )
+
+story003 = Story('003',
+                 'the Failing Student',
+                 ['fraction', 'adjective', 'noun-1', 'noun-2', 'verb-1', 'verb-2',
+                     'family member', 'emotion', 'possessive pronoun', 'plural noun'],
+                 'I failed the first {fraction} of a class in middle school, so I made a {adjective} {noun-1}. I did this every {fraction} that year. I forgot that they {verb-1} home the end-of-year {noun-1}s, and my {family member} got it before I could {verb-2} with my {adjective} one. My {family member} was {emotion}—at the school for their error. The {noun-2} also retired that year and had already thrown out {possessive pronoun} {plural noun}, so they had to take my {family member}’s “{noun-1}” (the {adjective} ones I made throughout the year) and “correct” the “mistake.” I’ve never told {subject pronoun} the truth.')
+
+STORIES = [story001, story002, story003]
 
 
-story = Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-)
+dict_stories_by_id = {story.story_id: story for story in STORIES}
